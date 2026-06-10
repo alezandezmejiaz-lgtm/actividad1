5e598686655af032a10f209000da0cd11e25ce8e
Luis Delgado <alezandezmejiaz@gmail.com>
2026-06-10 04:51:44 -0500

    index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import { sequelize } from './db/conexion.js';
+import EquipoRouter from './router/EquipoRouter.js';
+import { Equipo } from './model/EquipoModel.js'
+const app = express();
+const port = 3000;
+app.use('/api',EquipoRouter);
+
+const main = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync({ force:true});
+        console.log('Conexión establecida correctamente.');
+
+        app.listen(port, () => {
+            console.log(`Servidor corriendo en el puerto ${port}`);
+        });
+    } catch (error) {
+        console.error(`Error de conexión: ${error}`);
+    }
+};
+
+main();
