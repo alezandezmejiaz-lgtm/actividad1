c5665a383a013d026d4b5e2211b90ab697485646
Luis Delgado <alezandezmejiaz@gmail.com>
2026-06-10 06:59:58 -0500

    nuevo.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import EquipoRouter from './router/EquipoRouter.js';
 import { Equipo } from './model/EquipoModel.js'
 const app = express();
 const port = 3000;
+app.use(express.json());
 app.use('/api',EquipoRouter);
 
 const main = async () => {
