4682abfd9a9d20b093abbd78121a91ffe9271ce0
Luis Delgado <alezandezmejiaz@gmail.com>
2026-06-10 06:59:26 -0500

    metodo:postEquipo

--- router/EquipoRouter.js
+++ router/EquipoRouter.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import {getAllEquipos} from "../controller/EquipoController.js";
 import {getEquipoById } from "../controller/EquipoController.js";
+import {createEquipo } from "../controller/EquipoController.js";
 const router =Router();
 router.get('/equipos',getAllEquipos);
 router.get('/equipo/:id',getEquipoById);
+router.post('/equipo',createEquipo);
 export default router;
