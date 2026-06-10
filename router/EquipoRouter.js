8400730c4814af5f95cceab0e9ea0ed97b90bab2
Luis Delgado <alezandezmejiaz@gmail.com>
2026-06-10 05:49:33 -0500

    metodo EquipoRouter:getid

--- router/EquipoRouter.js
+++ router/EquipoRouter.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import {getAllEquipos} from "../controller/EquipoController.js";
+import {getEquipoById } from "../controller/EquipoController.js";
 const router =Router();
 router.get('/equipos',getAllEquipos);
-
+router.get('/equipo/:id',getEquipoById);
 export default router;
